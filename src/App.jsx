4dfa047fa1d46a271705dfa859163fdd78a0f286
Aandi1989 <navaliny1989@gmail.com>
2023-08-29 10:32:54 +0300

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ export const App = () => {
   return (
     <main className='relative'>
       <Nav/>
-      <section className='xl:padding-1 wide:padding-r padding-b'><Hero/></section>
+      <section className='xl:padding-l wide:padding-r padding-b'><Hero/></section>
       <section className='padding'><PopularProducts/></section>
       <section className='padding'><SuperQuality/></section>
       <section className='padding-x py-10'><Services/></section>
       <section className='padding'><SpecialOffer/></section>
       <section className='bg-pale-blue padding'><CustomerReviews/></section>
       <section className='padding-x sm:py-32 py-16 w-full'><Subscribe/></section>
-      <section className='bg-black padding-xpadding-t pb-8'><Footer/></section>
+      <section className='bg-black padding-x padding-t pb-8'><Footer/></section>
     </main>
   )
 }
